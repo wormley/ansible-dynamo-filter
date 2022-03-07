eb7d237dd72a1680b10da2a61aefd13da2c762f1
Steve Wormley <git@wormley.com>
2022-03-07 14:13:15 -0800

    Remove Excess Imports

--- filter/dynamo_ansible.py
+++ filter/dynamo_ansible.py
@@ -2,16 +2,6 @@ import json
 from boto3.dynamodb.types import TypeDeserializer, TypeSerializer
 
 
-from jinja2.filters import environmentfilter, do_groupby as _do_groupby
-
-from ansible.errors import AnsibleError, AnsibleFilterError, AnsibleFilterTypeError
-from ansible.module_utils.six.moves import shlex_quote
-from ansible.module_utils.six import string_types, integer_types, reraise
-from ansible.module_utils._text import to_bytes, to_native, to_text
-from ansible.module_utils.common.collections import is_sequence
-from ansible.module_utils.common._collections_compat import Mapping
-from ansible.parsing.ajson import AnsibleJSONEncoder
-
 class FilterModule(object):
     def filters(self):
         return {
@@ -28,7 +18,6 @@ class FilterModule(object):
 
 
     def dynamo_to_ansible(self,a):
-
         deserializer = TypeDeserializer()
         out = {
             k: deserializer.deserialize(v) 
